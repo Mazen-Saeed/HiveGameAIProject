c7f0e410938700adc926da2100532fb5aed08a72
Mazen-Saeed <youngbutold121@gmail.com>
2024-11-21 01:26:21 +0200

    working on the available pieces class

--- gameState.py
+++ gameState.py
@@ -9,38 +9,43 @@ class CellPosition:
         self.r = r  # Row value
 
 
-class GameState:
-    def __init__(self, player1Type, player2Type,player1Level,player2Level):
-        """
-        Initializes the game state.
-        :param player1Type: "p" if player 1 is a person, "c" if a computer
-        :param player2Type: "p" if player 2 is a person, "c" if a computer
-        :param player1Level: "e" for easy, "m" for medium, "h" for hard, "p" if player
-        :param player2Level: "e" for easy, "m" for medium, "h" for hard, "p" if player
-        """
-        self.player1Type = player1Type
-        self.player2Type = player2Type
-        self.player1Level = player1Level
-        self.player2Level = player2Level
-        self._initialize_state()
+class AvailablePieces:
+    def __init__(self):
+        self.g = 3
+        self.a = 3
+        self.b = 2
+        self.s = 2
+        self.q = 1
 
-    def _initialize_state(self):
-        """
-        A helper method to initialize or reset the state of the game.
-        Used by both the constructor and reset method.
-        """
+
+    def update_available_pieces(self,piece):
+        pass
+
+
+    def is_this_piece_available(self,piece):
+        pass
+
+class GameState:
+    def __init__(self, player1Type, player2Type, player1Level, player2Level):
+        self._initialize_state(player1Type, player2Type, player1Level, player2Level)
+
+
+    def _initialize_state(self,player1Type, player2Type,player1Level,player2Level):
         self.state = [["" for _ in range(50)] for _ in range(50)]  # Reset grid state
         self.q1 = CellPosition(-1, -1)  # Reset player 1's queen bee position
         self.q2 = CellPosition(-1, -1)  # Reset player 2's queen bee position
         self.turn = 1  # Reset turn to player 1
         self.player1Moves = 0  # Reset player 1's move count
         self.player2Moves = 0  # Reset player 2's move count
+        self.player1Type = player1Type # "p" if player 1 is a person, "c" if a computer
+        self.player2Type = player2Type # "p" if player 2 is a person, "c" if a computer
+        self.player1Level = player1Level # "e" for easy, "m" for medium, "h" for hard, "p" if player
+        self.player2Level = player2Level # "e" for easy, "m" for medium, "h" for hard, "p" if player
+        self.player1AvailablePieces = AvailablePieces()
+        self.player2AvailablePieces = AvailablePieces()
 
-    def reset(self):
-        """
-        Resets the game state to its initial configuration.
-        """
-        self._initialize_state()
+    def reset(self,player1Type, player2Type,player1Level,player2Level):
+        self._initialize_state(player1Type,player2Type,player1Level,player2Level)
 
 
     def make_a_move(self):
@@ -50,12 +55,14 @@ class GameState:
         """
         pass
 
+
     def get_allowed_moves(self):
         """
         Retrieves all allowed moves for the current player.
         Used when a person is playing.
         """
         pass
+
 
     def get_allowed_moves_given_a_cell(self, cell):
         """
@@ -64,11 +71,13 @@ class GameState:
         """
         pass
 
+
     def is_this_cell_ok(self,cell):
         """
         checks if the cell the player selected to move the piece in it is ok or not
         """
         pass
+
 
     def check_for_a_winning(self):
         """
@@ -76,6 +85,7 @@ class GameState:
         :return: "p1" if player 1 won, "p2" if player 2 won, or "c" if no player has won
         """
         pass
+
 
     def update_state(self, piece, from_cell, to_cell):
         """
@@ -85,7 +95,6 @@ class GameState:
         :param to_cell: CellPosition object representing the target position
         """
         pass
-
 
 """
  Pieces in the game:
