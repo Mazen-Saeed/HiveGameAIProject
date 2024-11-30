4a75a5b5389937fef61bedf807b98662ff0ffbcd
ahmed-khaled-abdelmaksod <ak4902280@gmail.com>
2024-11-30 21:35:53 +0200

    Solve font problem & style main buttons

--- GUI/Src/start_window.py
+++ GUI/Src/start_window.py
@@ -1,5 +1,5 @@
-from PyQt5.QtWidgets import QMainWindow , QApplication , QLabel ,QPushButton 
-from PyQt5.QtGui import QFont
+from PyQt5.QtWidgets import QMainWindow , QApplication , QLabel ,QPushButton,QFrame 
+from PyQt5.QtGui import QFont, QFontDatabase
 from PyQt5 import uic
 import sys
 import os
@@ -9,10 +9,11 @@ class StartWindow(QMainWindow):
         super(StartWindow,self).__init__()
 
         #load ui file
-        # print(os.getcwd()) # for debugging 
+        print(os.getcwd()) # for debugging 
         uic.loadUi("GUI/UI/start_window.ui",self)
 
 
+        
         
         # catch UI element from ui file 
         self.game_label = self.findChild(QLabel,"game_label")
@@ -20,7 +21,17 @@ class StartWindow(QMainWindow):
         self.hive_photo = self.findChild(QLabel,"hive_photo")
         self.start_game_button = self.findChild(QPushButton,"start_game_button")
         self.exit_button = self.findChild(QPushButton,"exit_button")
+        # self.player_one_frame = self.findChild(QFrame,"player_one_frame")
+        # self.player_one_diff_button = self.findChild(QPushButton,"player_one_diff_button")
 
+
+
+        # Load the custom font
+        self.load_font_needed()
+        
+        # Style connection
+        self.hive_label.setProperty("class", "hive-label") 
+        self.game_label.setProperty("class","game-label")
         # connect signal and slot
 
         # connect start button action 
@@ -47,9 +58,18 @@ class StartWindow(QMainWindow):
         # kill the program 
          QApplication.quit()
 
+    def load_font_needed(self):
+        font_id = QFontDatabase.addApplicationFont("GUI/Fonts/Pridi-Medium.ttf")
+        if font_id == -1:
+            print("Failed to load the font.")
+        else:
+            font_family = QFontDatabase.applicationFontFamilies(font_id)[0]
+            print(f"Loaded font: {font_family}")
+
 
 # TODO
 # Fix font problem
-# Style Buttons
+# Style Buttons (Done)
 # add Ai Options
 # Start BUtton Functionality
+# Fix directory Functionality
