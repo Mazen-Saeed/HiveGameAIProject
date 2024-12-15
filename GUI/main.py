9cf83293cfd7c5e31e9b69b865e4ec3467be50a4
Mariam Hesham <99722575+MariamH78@users.noreply.github.com>
2024-12-15 19:50:31 +0200

    added support for low res monitors (only for gameplay window, not startup window)

--- GUI/main.py
+++ GUI/main.py
@@ -1,9 +1,11 @@
+import sys
+import os
+sys.path.append("..")
+#os.chdir("/home/mazen/PycharmProjects/HiveGame/")
+
 from PyQt5.QtWidgets import QApplication
 from Src.start_window import StartWindow
 from Src.gameplay_window import GameplayWindow
-import sys
-import os
-#os.chdir("/home/mazen/PycharmProjects/HiveGame/")
 
 app = QApplication(sys.argv)
 StartWindow = StartWindow()
